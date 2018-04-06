270a52c3beed070d723ab9edd8a8b80d3ebadd4e
daniil volkov <ziimir9@gmail.com>
2018-04-06 17:32:27 +0300

    split quick sort fucntion into partition and sort functions

--- src/quick-sort/quick-sort.js
+++ src/quick-sort/quick-sort.js
@@ -1,14 +1,15 @@
-function quickSort(array, start, end) {
+function partition(array, start, end) {
     let leftBorder = start - 1;
     let rightBorder = start - 1;
     let pivotBorder = end;
+    let unknownBorder = start;
 
     let pivot = array[pivotBorder];
 
-    for (let i = start; i <= end - 1; i++) {
-        let current = array[i];
+    for (; unknownBorder <= end - 1; unknownBorder++) {
+        let current = array[unknownBorder];
 
-        if (array[i] <= pivot) {
+        if (current <= pivot) {
             rightBorder++;
             leftBorder++;
 
@@ -26,12 +27,16 @@ function quickSort(array, start, end) {
     pivotBorder = leftBorder + 1;
 
     if ((pivotBorder - 1) - start > 0) {
-        quickSort(array, start, pivotBorder - 1);
+        partition(array, start, pivotBorder - 1);
     }
 
     if (end - (pivotBorder + 1) > 0) {
-        quickSort(array, pivotBorder + 1, end);
+        partition(array, pivotBorder + 1, end);
     }
+}
+
+function quickSort(array, length) {
+    partition(array, 0, length - 1);
 
     return array;
 }
--- src/quick-sort/quick-sort.test.js
+++ src/quick-sort/quick-sort.test.js
@@ -6,7 +6,7 @@ describe('Quick sort', () => {
         let unsortedArray = [9, 2, 5, 10, 1, 6, 7, 3, 8, 4, 9, 0];
         let sortedArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 10];
 
-        let result = quickSort(unsortedArray, 0, unsortedArray.length - 1);
+        let result = quickSort(unsortedArray, unsortedArray.length);
 
         expect(result).to.eql(sortedArray);
     });
