af81c6a9cc87aee08386ae955192d64a7a79f4de
daniil volkov <ziimir9@gmail.com>
2018-04-09 17:00:29 +0300

    fix selection sort

--- src/selection-sort/selection-sort.js
+++ src/selection-sort/selection-sort.js
@@ -5,7 +5,7 @@ function selectionSort(arr, n) {
     for (let i = 0; i < n - 1; i++) {
         let smallest = i;
 
-        for (let j = i + 1; j <= n; j++) {
+        for (let j = i + 1; j < n; j++) {
             if (arr[j] < arr[smallest]) {
                 smallest = j;
             }
