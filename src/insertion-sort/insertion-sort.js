3dceee27b1ded82849e77cca87babc0163748b36
daniil volkov <ziimir9@gmail.com>
2018-04-09 16:20:16 +0300

    simplify insertion sort

--- src/insertion-sort/insertion-sort.js
+++ src/insertion-sort/insertion-sort.js
@@ -2,16 +2,17 @@
 function insertionSort(arr, n) {
     arr = arr.slice();
 
-    for (i = 1; i < n; i++) {
-        let value = arr[i];
+    for (let i = 1; i < n; i++) {
+        let key = arr[i];
         let j = i - 1;
 
-        while (j >= 0 && value < arr[j]) {
+        while (j >= 0 && key < arr[j]) {
             arr[j + 1] = arr[j];
-            j = j - 1;
+
+            j--;
         }
 
-        arr[j + 1] = value;
+        arr[j + 1] = key;
     }
 
     return arr;
