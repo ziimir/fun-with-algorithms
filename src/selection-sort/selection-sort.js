985e6ac3ffb11b91fff1d6f1c1b03b81c6a9562a
daniil volkov <ziimir9@gmail.com>
2018-04-09 14:27:25 +0300

    simplify selection sort

--- src/selection-sort/selection-sort.js
+++ src/selection-sort/selection-sort.js
@@ -3,13 +3,17 @@ function selectionSort(arr, n) {
     arr = arr.slice();
 
     for (let i = 0; i < n - 1; i++) {
-        for (let j = i + 1; j < n; j++) {
-            if (arr[j] < arr[i]) {
-                let tmp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = tmp;
+        let smallest = i;
+
+        for (let j = i + 1; j <= n; j++) {
+            if (arr[j] < arr[smallest]) {
+                smallest = j;
             }
         }
+
+        let tmp = arr[i];
+        arr[i] = arr[smallest];
+        arr[smallest] = tmp;
     }
 
     return arr;
